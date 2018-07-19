0dee613afdc2bbd7977b8708cb39fc311182c849
Bruno FS <fsouza.bruno@gmail.com>
2018-07-19 20:29:33 -0300

    atualiza script entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -e
 
-# Alters user uid/gid to match TARGET_DIR
-DIR=${TARGET_DIR:-.} # fallback to workdir
+# Alters user uid/gid to match TARGET_DIR / curr dir
+DIR=${TARGET_DIR:-.}
 TARGET_GID=$(stat -c "%g" ${DIR})
 TARGET_UID=$(stat -c "%u" ${DIR})
 
 EXISTS=$(cat /etc/group | grep ${TARGET_GID} | wc -l)
 # Create new group using target GID and add nobody user
   if [ $EXISTS == "0" ]; then
-    groupadd -g $TARGET_GID tempgroup
-    sudo usermod -ag tempgroup sudo ubuntu
+    sudo groupmod -g $TARGET_GID ubuntu
+    sudo usermod -u ${TARGET_UID} ubuntu
   else
     # GID exists, find group name and add
     GROUP=$(getent group $TARGET_GID | cut -d: -f1)
-    sudo usermod -aG $GROUP ubuntu
+    sudo usermod -aG $GROUP -u ${TARGET_UID} ubuntu
   fi
-# now executes whatever the user passes
 "$@"
